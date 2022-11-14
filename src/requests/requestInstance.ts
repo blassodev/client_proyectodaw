c6992ae6c23395dfb05d8b87c56162480b3a5e96
Blas Santomé Ocampo <bsantome@mercadona.es>
2022-11-14 21:27:04 +0100

    chore(instance): Add real backend

--- src/requests/requestInstance.ts
+++ src/requests/requestInstance.ts
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 export const requestInstance = axios.create({
-  baseURL: 'http://localhost:4000/',
+  baseURL: 'https://daw.blasserver.com/',
   headers: {
     Authorization: 'Bearer ' + localStorage.getItem('token'),
   },
 });
 
 export const requestNoTokenInstance = axios.create({
-  baseURL: 'http://localhost:4000/',
+  baseURL: 'https://daw.blasserver.com/',
 });
