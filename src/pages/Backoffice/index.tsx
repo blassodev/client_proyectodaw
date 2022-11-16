c603fc0387bbd7f5442a843c952e14bd87d9c3a2
Blas Santomé Ocampo <bsantome@mercadona.es>
2022-11-16 21:53:21 +0100

    feat(backoffice): add header

--- src/pages/Backoffice/index.tsx
+++ src/pages/Backoffice/index.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { Menubar } from 'primereact/menubar';
+import { MenuItem } from 'primereact/menuitem';
+import { useAuth } from '../../hooks/useAuth';
 
 export const Backoffice: React.FC = () => {
+  const auth = useAuth();
+
+  const items: MenuItem[] = [
+    {
+      label: 'Peliculas',
+      url: '/backoffice/movies',
+      icon: 'pi pi-play',
+    },
+    {
+      label: 'Directores',
+      url: '/backoffice/directors',
+      icon: 'pi pi-user',
+    },
+    {
+      label: 'Cerrar sesión',
+      icon: 'pi pi-power-off',
+      command() {
+        auth.signout(() => {});
+      },
+    },
+  ];
   return (
     <div>
-      <p>Header</p>
+      <Menubar model={items} />
       <Outlet />
     </div>
   );
